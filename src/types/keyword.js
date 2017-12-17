3285e603d83ff0174c628d8d4f055fba962e0783
Gabe Pearhill <gabepearhill@gmail.com>
2017-12-17 00:12:50 -0700

    Commit missing file!

---
+++ src/types/keyword.js
@@ -0,0 +1,34 @@
+const _ = require('lodash');
+const Table = require('./table');
+const argUtils = require('./utils/argUtils');
+
+let setMode = {
+  args: argUtils.args('value'),
+  _eval: (self, args, ns, tableEval, types, method) => {
+    if (_.get(args[1], 'isKeyword', false))
+      _.set(self, _.get(method, 'value'), _.get(args[0], 'value'));
+    else _.set(self, _.get(method, 'value'), args[0]);
+    return self;
+  }
+};
+
+let getMode = {
+  args: argUtils.args(),
+  _eval: (self, args, ns, tableEval, types, method) => {
+    return _.get(self, _.get(method, 'value'), null);
+  }
+};
+
+let methods = {
+  isKeyword: {
+    args: argUtils.args(),
+    _eval: (self, args, ns, tableEval, types) => {
+      return new types.Boolean(true);
+    }
+  }
+};
+
+module.exports = {
+  Get: Table(_.assign({}, methods, getMode)),
+  Set: Table(_.assign({}, methods, setMode))
+};
